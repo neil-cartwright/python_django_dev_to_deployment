2b8072d20bee99baecba1be1a137da8b4764fda5
neil-cartwright <neil@sitepages.co.uk>
2020-12-02 10:00:41 +0000

    picked up on second december 2020

--- btre/urls.py
+++ btre/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
